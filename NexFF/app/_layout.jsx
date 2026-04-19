59c9b37a82485d0c1abccd6a508a8afed6081955
Arjun490209 <colorfulz099@gmail.com>
2026-04-19 23:10:12 +0530

    Fixed NexFF folder issue

---
+++ NexFF/app/_layout.jsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+import "../global.css";
+import Toast from "react-native-toast-message";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Toast />
+    </Stack>
+  );
+}
