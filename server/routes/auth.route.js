88b12c68e399596c8ea53f58de3bcb6101c8fb60
Arjun490209 <colorfulz099@gmail.com>
2026-05-08 07:00:26 +0530

    "fixed auth controller routes"

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,16 +1,19 @@
 import e from "express";
 import auth from "../controllers/auth.controller.js";
 import protect from "../middleware/Protect.js";
+
 const router = e.Router();
 
 router.post("/register", auth.register);
+
 router.post("/login", auth.login);
-router.get("/users", protect, getAllUsers);
 
-router.put("/user/role/:id", protect, updateUserRole);
+router.get("/users", protect, auth.getAllUsers);
 
-router.put("/user/wallet/:id", protect, addWalletBalance);
+router.put("/user/role/:id", protect, auth.updateUserRole);
 
-router.delete("/user/:id", protect, deleteUser);
+router.put("/user/wallet/:id", protect, auth.addWalletBalance);
+
+router.delete("/user/:id", protect, auth.deleteUser);
 
 export default router;
